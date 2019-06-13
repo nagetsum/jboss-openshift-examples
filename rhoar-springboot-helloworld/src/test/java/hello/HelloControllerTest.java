a8655ad6a6d33a601eed5256796e8f9363435309
Norito Agetsuma <norito.agetsuma@gmail.com>
2019-06-13 17:40:24 +0900

    test code

---
+++ rhoar-springboot-helloworld/src/test/java/hello/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testIndex() throws Exception {
+        this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk()).andExpect(content().string("Greetings from Spring Boot!"));
+    }
+}
