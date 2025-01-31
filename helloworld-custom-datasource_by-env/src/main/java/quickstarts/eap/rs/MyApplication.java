56560029c359dc96e70f5e24f40f47d312e154e0
Norito Agetsuma <nagetsum@redhat.com>
2025-01-31 15:07:33 +0900

    delete as outdated

--- helloworld-custom-datasource_by-env/src/main/java/quickstarts/eap/rs/MyApplication.java
+++
@@ -1,8 +0,0 @@
-package quickstarts.eap.rs;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class MyApplication extends Application {
-}
